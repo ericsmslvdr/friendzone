cd20675972a7beff2f7d521cdce3210e8dea17f3
Eric Samuel Salvador <ericsmsalvador@gmail.com>
2023-12-07 22:54:12 +0800

    remove body-parser and use built-in from express

--- server/index.js
+++ server/index.js
@@ -17,8 +17,8 @@ try {
 
 const app = express()
 
-app.use(bodyParser.json)
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 const corsOptions = {
     origin: '*',
