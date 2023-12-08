73ddb89437e234a7c76da9a09b4f33bd83ac6477
Eric Samuel Salvador <ericsmsalvador@gmail.com>
2023-12-08 12:13:59 +0800

    work navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 
 const Navbar = () => {
     return (
-        <nav className="py-2 px-20 flex justify-between items-center">
-            <div>
-                FriendZone
-            </div>
-            <div>
-                <button
-                    type="button"
-                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2 dark:bg-red-600 dark:hover:bg-red-700"
-                >
-                    Logout
-                </button>
+        <nav className="container mx-auto">
+            <div className="flex justify-between items-center py-2">
+                <div>
+                    FriendZone
+                </div>
+                <div>
+                    <button
+                        type="button"
+                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2 dark:bg-red-600 dark:hover:bg-red-700"
+                    >
+                        Logout
+                    </button>
+                </div>
             </div>
         </nav>
     )
